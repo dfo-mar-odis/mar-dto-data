e3a3ee67780da9efa0ada81f739383141f409c91
Remi Daigle <remi-daigle@users.noreply.github.com>
2026-04-20 15:39:24 -0300

    flipping onsets and  trends

--- _targets.R
+++ _targets.R
@@ -56,6 +56,54 @@ regularize_stars <- function(s) {
   return(s)
 }
 
+
+raster_time_trend <- function(
+  stars_obj,
+  time_dim = "year",
+  lat_dim = "latitude",
+  lon_dim = "longitude"
+) {
+  time_numeric <- as.numeric(st_get_dimension_values(
+    stars_obj,
+    time_dim
+  ))
+
+  data_array <- stars_obj[[1]]
+
+  result_array <- apply(data_array, c(1, 2), function(y) {
+    valid <- !is.na(y)
+    if (sum(valid) < 3) {
+      return(c(NA_real_, NA_real_))
+    }
+
+    fit <- .lm.fit(cbind(1, time_numeric[valid]), y[valid])
+    n <- sum(valid)
+    residuals <- y[valid] - cbind(1, time_numeric[valid]) %*% fit$coefficients
+    rss <- sum(residuals^2)
+
+    se <- sqrt(
+      (rss / (n - 2)) /
+        sum((time_numeric[valid] - mean(time_numeric[valid]))^2)
+    )
+    t_stat <- fit$coefficients[2] / se
+    pval <- 2 * pt(abs(t_stat), df = n - 2, lower.tail = FALSE)
+
+    c(fit$coefficients[2], pval)
+  })
+
+  slope_array <- result_array[1, , ]
+  pvalue_array <- result_array[2, , ]
+
+  # convert back to stars
+  result <- st_as_stars(
+    list(slope = slope_array, pvalue = pvalue_array),
+    dimensions = st_dimensions(stars_obj)[c(
+      lon_dim,
+      lat_dim
+    )]
+  )
+}
+
 # Set target options:
 tar_option_set(
   packages = pkgs
@@ -387,7 +435,7 @@ list(
           if (all(is.na(x))) {
             rep(NA_real_, length(x))
           }
-          anomlies <- x - mean(x[1:30], na.rm = TRUE)
+          anomlies <- mean(x[1:30], na.rm = TRUE) - x
           anomlies / 7
         },
         .fname = "year"
@@ -473,7 +521,7 @@ list(
           if (all(is.na(x))) {
             rep(NA_real_, length(x))
           }
-          anomlies <- x - mean(x[1:30], na.rm = TRUE)
+          anomlies <- mean(x[1:30], na.rm = TRUE) - x
           anomlies / 7
         },
         .fname = "year"
@@ -531,44 +579,41 @@ list(
       ) |>
       aperm(c("longitude", "latitude", "year"))
 
-    time_numeric <- as.numeric(st_get_dimension_values(
-      annualmean,
-      "year"
-    ))
+    raster_time_trend(annualmean)
+  }),
 
-    data_array <- annualmean[[1]]
-
-    result_array <- apply(data_array, c(1, 2), function(y) {
-      valid <- !is.na(y)
-      if (sum(valid) < 3) {
-        return(c(NA_real_, NA_real_))
-      }
-
-      fit <- .lm.fit(cbind(1, time_numeric[valid]), y[valid])
-      n <- sum(valid)
-      residuals <- y[valid] - cbind(1, time_numeric[valid]) %*% fit$coefficients
-      rss <- sum(residuals^2)
-
-      se <- sqrt(
-        (rss / (n - 2)) /
-          sum((time_numeric[valid] - mean(time_numeric[valid]))^2)
-      )
-      t_stat <- fit$coefficients[2] / se
-      pval <- 2 * pt(abs(t_stat), df = n - 2, lower.tail = FALSE)
-
-      c(fit$coefficients[2], pval)
-    })
-
-    slope_array <- result_array[1, , ]
-    pvalue_array <- result_array[2, , ]
-
-    # convert back to stars
-    result <- st_as_stars(
-      list(slope = slope_array, pvalue = pvalue_array),
-      dimensions = st_dimensions(annualmean)[c(
-        "longitude",
-        "latitude"
-      )]
+  tar_target(name = ind_glorys_sst_trend, command = {
+    annualmean <- aggregate(
+      data_glorys_sst,
+      by = "year",
+      FUN = mean,
+      na.rm = TRUE
     )
+
+    annualmean <- annualmean |>
+      st_set_dimensions(
+        "time",
+        names = "year",
+        st_get_dimension_values(annualmean, "time") |>
+          format("%Y") |>
+          as.integer()
+      ) |>
+      aperm(c("longitude", "latitude", "year"))
+
+    raster_time_trend(annualmean)
+  }),
+
+  tar_target(name = ind_onset_of_spring_trend, command = {
+    raster_time_trend(data_onset_of_spring["Onset of Spring Anomaly (Weeks)"])
+  }),
+
+  tar_target(name = ind_onset_of_fall_trend, command = {
+    raster_time_trend(data_onset_of_fall["Onset of Fall Anomaly (Weeks)"])
+  }),
+
+  tar_target(name = ind_length_growing_season_trend, command = {
+    raster_time_trend(data_length_growing_season[
+      "Length of the Growing Season Anomaly (Weeks)"
+    ])
   })
 )
